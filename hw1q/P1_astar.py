affc6e6ff1e23e6f959ec8dbc9d3f2504d6e7153
Quang Doan <quang.m.doan@gmail.com>
2025-11-08 23:26:53 -0800

    Clean up

--- hw1q/P1_astar.py
+++ hw1q/P1_astar.py
@@ -4,7 +4,6 @@ import matplotlib.patches as patches
 from utils import plot_line_segments
 
 class AStar(object):
-    """Represents a motion planning problem to be solved using A*"""
 
     def __init__(self, statespace_lo, statespace_hi, x_init, x_goal, occupancy, resolution=1):
         self.statespace_lo = statespace_lo         # state space lower bound (e.g., [-5, -5])
@@ -29,82 +28,27 @@ class AStar(object):
         self.path = None        # the final path as a list of states
 
     def is_free(self, x):
-        """
-        Checks if a give state x is free, meaning it is inside the bounds of the map and
-        is not inside any obstacle.
-        Inputs:
-            x: state tuple
-        Output:
-            Boolean True/False
-        Hint: self.occupancy is a DetOccupancyGrid2D object, take a look at its methods for what might be
-              useful here
-        """
-        ########## Code starts here ##########
-        ########## Code ends here ##########
+        # TODO 
+        return True
 
     def distance(self, x1, x2):
-        """
-        Computes the Euclidean distance between two states.
-        Inputs:
-            x1: First state tuple
-            x2: Second state tuple
-        Output:
-            Float Euclidean distance
-
-        HINT: This should take one line. Tuples can be converted to numpy arrays using np.array().
-        """
-        ########## Code starts here ##########
-        ########## Code ends here ##########
-
+        return np.linalg.norm(np.array(x1) - np.array(x2))
+    
     def snap_to_grid(self, x):
-        """ Returns the closest point on a discrete state grid
-        Input:
-            x: tuple state
-        Output:
-            A tuple that represents the closest point to x on the discrete state grid
-        """
         return (
             self.resolution * round((x[0] - self.x_offset[0]) / self.resolution) + self.x_offset[0],
             self.resolution * round((x[1] - self.x_offset[1]) / self.resolution) + self.x_offset[1],
         )
 
     def get_neighbors(self, x):
-        """
-        Gets the FREE neighbor states of a given state x. Assumes a motion model
-        where we can move up, down, left, right, or along the diagonals by an
-        amount equal to self.resolution.
-        Input:
-            x: tuple state
-        Ouput:
-            List of neighbors that are free, as a list of TUPLES
-
-        HINTS: Use self.is_free to check whether a given state is indeed free.
-               Use self.snap_to_grid (see above) to ensure that the neighbors
-               you compute are actually on the discrete grid, i.e., if you were
-               to compute neighbors by adding/subtracting self.resolution from x,
-               numerical errors could creep in over the course of many additions
-               and cause grid point equality checks to fail. To remedy this, you
-               should make sure that every neighbor is snapped to the grid as it
-               is computed.
-        """
-        ########## Code starts here ##########
-        ########## Code ends here ##########
-        return None
+        # TODO
+        neighbors = []
+        return neighbors
 
     def find_best_est_cost_through(self):
-        """
-        Gets the state in open_set that has the lowest est_cost_through
-        Output: A tuple, the state found in open_set that has the lowest est_cost_through
-        """
         return min(self.open_set, key=lambda x: self.est_cost_through[x])
 
     def reconstruct_path(self):
-        """
-        Use the came_from map to reconstruct a path from the initial location to
-        the goal location
-        Output:
-            A list of tuples, which is a list of the states that go from start to goal
-        """
         path = [self.x_goal]
         current = path[-1]
         while current != self.x_init:
@@ -113,7 +57,6 @@ class AStar(object):
         return list(reversed(path))
 
     def plot_path(self, fig_num=0, show_init_label=True):
-        """Plots the path found in self.path and the obstacles"""
         if not self.path:
             return
 
@@ -137,35 +80,16 @@ class AStar(object):
         plt.scatter(px, py, color="blue", s=point_size, zorder=10, alpha=0.2)
 
     def solve(self):
-        """
-        Solves the planning problem using the A* search algorithm. It places
-        the solution as a list of tuples (each representing a state) that go
-        from self.x_init to self.x_goal inside the variable self.path
-        Input:
-            None
-        Output:
-            Boolean, True if a solution from x_init to x_goal was found
-
-        HINTS:  We're representing the open and closed sets using python's built-in
-                set() class. This allows easily adding and removing items using
-                .add(item) and .remove(item) respectively, as well as checking for
-                set membership efficiently using the syntax "if item in set".
-        """
-        ########## Code starts here ##########
-        ########## Code ends here ##########
+        # TODO
+        return False
 
 class DetOccupancyGrid2D(object):
-    """
-    A 2D state space grid with a set of rectangular obstacles. The grid is
-    fully deterministic
-    """
     def __init__(self, width, height, obstacles):
         self.width = width
         self.height = height
         self.obstacles = obstacles
 
     def is_free(self, x):
-        """Verifies that point is not inside any obstacles by some margin"""
         for obs in self.obstacles:
             if x[0] >= obs[0][0] - self.width * .01 and \
                x[0] <= obs[1][0] + self.width * .01 and \
@@ -175,7 +99,6 @@ class DetOccupancyGrid2D(object):
         return True
 
     def plot(self, fig_num=0):
-        """Plots the space and its obstacles"""
         fig = plt.figure(fig_num)
         ax = fig.add_subplot(111, aspect='equal')
         for obs in self.obstacles:
---
+++ hw1q/readme.txt
@@ -0,0 +1,11 @@
+cd ~/cs237-hws/hw1q
+
+uv venv --python 3.12
+
+source .venv/bin/activate
+uv pip install numpy
+uv pip install ipykernel
+uv pip install matplotlib
+uv pip install scipy
+
+code .

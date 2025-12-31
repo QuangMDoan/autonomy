c3fc3cc10393bd59588f6e296df2642083ab3110
Quang Doan <quang.m.doan@gmail.com>
2025-12-30 17:55:33 -0800

    lab3 starter

---
+++ lab3/scripts/constant_control.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+
+import rclpy
+from rclpy.node import Node
+from std_msgs.msg import Bool
+from geometry_msgs.msg import Twist 
+
+
+class MoveStraightLine(Node):
+    def __init__(self):
+        super().__init__("movestraightline")
+        self.v = 0.5
+        self.theta = 0.0
+
+        self.publisher = self.create_publisher(Twist, "/cmd_vel", 10)
+        self.hb_timer = self.create_timer(1.0, self.hb_callback)
+        self.motor_sub = self.create_subscription(Bool, "/kill", self.kill_callback, 10)
+
+    def hb_callback(self) -> None:
+        msg = Twist()
+        msg.linear.x = self.v
+        msg.angular.z = self.theta
+
+        self.publisher.publish(msg)
+
+    def kill_callback(self, msg: Bool):
+        if not msg.data:
+            msg = Twist()
+            self.publisher.publish(msg)
+            
+            self.get_logger().fatal("heartbeat stopped")
+
+            self.hb_timer.cancel()
+
+if __name__ == "__main__":
+    # initialize ROS2 context 
+    rclpy.init()
+    node = MoveStraightLine()
+    rclpy.spin(node)
+    rclpy.shutdown()

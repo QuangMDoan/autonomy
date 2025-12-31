2b76ede3ddd3c65961e92a2b2bdc43b72f659204
Quang Doan <quang.m.doan@gmail.com>
2025-12-30 17:12:23 -0800

    Change the heartbeat to send Twist msgs to /cmd_vel topics

--- lab2/scripts/constant_control.py
+++ lab2/scripts/constant_control.py
@@ -2,26 +2,34 @@
 
 import rclpy
 from rclpy.node import Node
-from std_msgs.msg import Int64, Bool
+from std_msgs.msg import Bool
+from geometry_msgs.msg import Twist 
+
 
 class HBeat(Node):
     def __init__(self):
         super().__init__("heartbeat")
-        self.count :Int64 = 0
+        self.v = 0.5
+        self.theta = 0.2
 
-        self.publisher = self.create_publisher(Int64, "/heartbeat", 10)
+        self.publisher = self.create_publisher(Twist, "/cmd_vel", 10)
         self.hb_timer = self.create_timer(1.0, self.hb_callback)
         self.motor_sub = self.create_subscription(Bool, "/health/motor", self.health_callback, 10)
 
     def hb_callback(self) -> None:
-        msg = Int64()
-        msg.data = self.count
+        msg = Twist()
+        msg.linear.x = self.v
+        msg.angular.z = self.theta
+
         self.publisher.publish(msg)
-        self.count += 1
 
     def health_callback(self, msg: Bool):
         if not msg.data:
+            msg = Twist()
+            self.publisher.publish(msg)
+            
             self.get_logger().fatal("heartbeat stopped")
+
             self.hb_timer.cancel()
 
 if __name__ == "__main__":

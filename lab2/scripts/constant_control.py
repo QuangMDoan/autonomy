d2fe1d89348a93d15aa37a3ebcbc3f77a8c71d58
Quang Doan <quang.m.doan@gmail.com>
2025-12-30 16:33:35 -0800

    Lab 2 task 3 - publish constant velocity

--- lab2/scripts/constant_control.py
+++ lab2/scripts/constant_control.py
@@ -3,18 +3,20 @@
 import rclpy
 from rclpy.node import Node
 from std_msgs.msg import String
+from geometry_msgs.msg import Twist
 
 class ConstantControlNode(Node):
     def __init__(self) -> None: 
         super().__init__("const_ctrl_node")
         self.msg_ctr = 0
         self.const_timer = self.create_timer(0.2, self.timer_callback)
-        self.ctrl_pub = self.create_publisher(String, "/const_ctrl", 10)
+        self.ctrl_pub = self.create_publisher(Twist, "/cmd_vel", 10)
 
     def timer_callback(self) -> None: 
         self.msg_ctr += 1
-        msg = String()
-        msg.data = "sending constant control %d.." % self.msg_ctr
+        msg = Twist() # 0 initialized by default
+        msg.linear.x = 2.0
+        msg.angular.z = 2.0
         self.ctrl_pub.publish(msg)
 
 if __name__ == "__main__":
